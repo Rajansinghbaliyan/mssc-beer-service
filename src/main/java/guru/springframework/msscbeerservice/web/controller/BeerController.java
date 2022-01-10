cc73b25e135c51584ed46162051e41efde200a50
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-10 12:09:02 +0530

    feat: added the beer controller

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,53 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.services.BeerService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+    private final BeerService beerService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<BeerDto>> findAll(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(beerService.findAll());
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<BeerDto> getById(@NotNull @PathVariable String uuid) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(beerService.getById(UUID.fromString(uuid)));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(beerService.saveNewBeer(beerDto));
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable String beerId, @Validated @RequestBody BeerDto beerDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(beerService.updateBeer(UUID.fromString(beerId), beerDto));
+    }
+}

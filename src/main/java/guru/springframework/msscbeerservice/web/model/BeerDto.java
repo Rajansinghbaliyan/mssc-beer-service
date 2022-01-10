27ed65361f8840b38a3baafea9a23a17fd7ad122
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-10 08:28:13 +0530

    feat: added validation constraints

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -1,20 +1,40 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime creationDate;
+    @Null
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModificationDate;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
+    @Positive
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
 
+    @NotNull
     private BeerStyle beerStyle;
 
+    @NotBlank
     private String upc;
 
 

19f00c5bfb3331f446bf3805c037b26ff89e0aed
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-11 21:47:34 +0530

    feat: update of the mapper and added without inventory

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mapper/BeerMapperDecorator.java
@@ -0,0 +1,48 @@
+package guru.springframework.msscbeerservice.web.mapper;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.services.inventory.services.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@NoArgsConstructor
+public abstract class BeerMapperDecorator implements BeerMapper {
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper mapper;
+
+    @Autowired
+    public void setBeerInventoryService(BeerInventoryService beerInventoryService) {
+        this.beerInventoryService = beerInventoryService;
+    }
+
+    @Autowired
+    public void setMapper(BeerMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto) {
+        return mapper.beerDtoToBeer(beerDto);
+    }
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        BeerDto dto = mapper.beerToBeerDto(beer);
+        dto.setQuantityOnHand(beerInventoryService.getOnHandQuantityByBeerId(dto.getId()));
+
+        log.debug("The Beer get Id:" + dto.getId());
+        return dto;
+    }
+
+    @Override
+    public BeerDto beerToDtoWithOutInventory(Beer beer) {
+        BeerDto dto = mapper.beerToBeerDto(beer);
+        dto.setQuantityOnHand(null);
+        return dto;
+    }
+}

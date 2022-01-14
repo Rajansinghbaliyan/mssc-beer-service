72d08c506644929417b0eb1ad407926ed8d5b97f
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-14 15:52:47 +0530

    feat: added jms configuration

---
+++ src/main/java/guru/springframework/msscbeerservice/config/jms/JmsConfig.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbeerservice.config.jms;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+@Getter
+public class JmsConfig {
+
+    public static final String BREWERY_QUEUE = "brewery-queue";
+
+    @Bean
+    public MessageConverter converter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/sender/BreweryStarter.java
@@ -0,0 +1,31 @@
+package guru.springframework.msscbeerservice.sender;
+
+import guru.springframework.msscbeerservice.config.jms.JmsConfig;
+import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class BreweryStarter {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 3000)
+    public void startBrewring(){
+        BeerInventoryDto dto = BeerInventoryDto.builder()
+                .beerId(UUID.randomUUID())
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.BREWERY_QUEUE,dto);
+        log.info("Brewing initiated for Beer Id: "+dto.getBeerId());
+    }
+
+}

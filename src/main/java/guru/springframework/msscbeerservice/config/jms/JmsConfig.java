e1c8e5e7a5a3d8bcd50ada6b269f30558e9644f2
rbaliyancosm <87328960+rbaliyancosm@users.noreply.github.com>
2022-01-21 08:17:03 +0530

    feat: added new queues

--- src/main/java/guru/springframework/msscbeerservice/config/jms/JmsConfig.java
+++ src/main/java/guru/springframework/msscbeerservice/config/jms/JmsConfig.java
@@ -11,6 +11,10 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
     public static final String BREW_BEER_QUEUE = "brew-beer";
     public static final String BEER_INVENTORY_QUEUE = "beer-inventory";
+    public static final String VALIDATE_QUEUE = "validate_queue";
+    public static final String VALIDATION_SUCCESS_QUEUE = "validation_success_queue";
+    public static final String VALIDATION_FAILED_QUEUE = "validation_failed_queue";
+    public static final String VALIDATION_BUT_NO_INVENTORY_QUEUE     = "validation_but_no_inventory_queue";
 
     @Bean
     public MessageConverter messageConverter(ObjectMapper objectMapper) {
